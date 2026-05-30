a08198c1cd4c49e4996600a097cbac77320dc0ee
mohana-python <mohanavemula04@gmail.com>
2026-05-30 17:35:23 -0500

    README.md
    # Python Beginner Journey
    
    This repository contains my daily Python learning practice.
    
    ## Goals
    - Learn Python step by step
    - Build real projects
    - Become job-ready for Data Analyst / Python Developer roles
    
    ## About Me
    Computer Science graduate currently learning Python in USA.  
    Authorized to work (EAD).
    
    ## Progress
    Day 2: Input, Output, Variables completed

---
+++ day1.py
@@ -0,0 +1,5 @@
+name = "Mohana"
+goal = "Get an IT job in USA"
+
+print("My name is", name)
+print("My goal is", goal)
---
+++ strings.py
@@ -0,0 +1,22 @@
+character_name = "Ishika"
+age = 1
+is_female =True
+print("Once upon a time, a girl named "+ character_name +",")
+print("Her age is just "+ str(age) +".")
+print("She is a Female:"+str(is_female))
+
+phrase="Giraffe Academy "
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
+print("Giraffe\Academy")
+print(phrase + "is cool")
+print(len(phrase))
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(phrase[0])
+print(phrase[3])
+print(phrase.index("G"))
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe","Elephant"))
