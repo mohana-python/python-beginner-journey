021b18f3a87ffe8e06087ef4758e7dec77ed381e
mohana-python <mohanavemula04@gmail.com>
2026-06-01 17:39:43 -0500

    README.md

---
+++ forloop.py
@@ -0,0 +1,16 @@
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends=["jim","karen","kevin"]
+for friend in friends:
+    print(friend)
+
+
+for index in range(10):
+    print(index)
+for index in range(3,10):
+    print(index)
+
+friends=["jim","karen","kevin"]
+for index in range(len(friends)):
+    print(friends[index])
---
+++ whileloop.py
@@ -0,0 +1,11 @@
+i = 1
+while i <= 10:
+    print(i)
+    i += 1   # i = i+1
+
+print("Done with loop")
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
