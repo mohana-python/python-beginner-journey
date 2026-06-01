848f20eec9c63eb01540330999ddd61a58c730a7
mohana-python <mohanavemula04@gmail.com>
2026-06-01 16:44:44 -0500

    README.md
    # Python Beginner Journey
    
    Learning Python from scratch and building projects.
    
    Current Topics:
    - for loops
    - while loops
    - Examples and Practices

---
+++ practice.py
@@ -0,0 +1,12 @@
+for i  in range(1,11):
+    print(i)
+
+for i in range(5):
+    print("Mohana")
+
+for i in range(10,0,-1):
+    print(i)
+
+number=int(input("Enter a number: "))
+for i in range(1,11):
+    print(number,"*",i,"=",number*i)
