9b5f2589abd8d209a85a75c7e35911335949942d
mohana-python <mohanavemula04@gmail.com>
2026-06-01 17:19:08 -0500

    Rename dreamjob.py to day01/dreamjob.py

---
+++ day01/dreamjob.py
@@ -0,0 +1,3 @@
+dream_job="Software Engineer"
+country="USA"
+print("I will become a "+dream_job+" in "+country)
--- dreamjob.py
+++
@@ -1,3 +0,0 @@
-dream_job="Software Engineer"
-country="USA"
-print("I will become a "+dream_job+" in "+country)
